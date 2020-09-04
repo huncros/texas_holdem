72643cd12220e3241e80a2a60b1f59e9c47d4983
David Herskovics <huncros@gmail.com>
2020-09-04 23:29:46 +0200

    compare hands

---
+++ src/texas_holdem/compare_hands.py
@@ -0,0 +1,59 @@
+'Compares your hand with those of your opponents.'
+from texas_holdem.card import HoleCards, Board
+from texas_holdem.evaluate_hand import sort_hand_ranks_in_incr_order, HandRank, HandValue, \
+    HandValueWhenRankIsFixedAs
+
+# Some hand ranks' criteria also includes the criteria for weaker hand ranks.
+# E.g. a FOUR_OF_A_KIND also satisfies the criteria for TWO_PAIRS, only with the the pairs being
+# of the same rank.
+# As a consequence, if we had checked that cards available to the player don't contain TWO_PAIRS
+# then we also know that they don't contain FOUR_OF_A_KIND (or with the same logic: FULL_HOUSE)
+# either. This can be used to decrease the computation needed when comparing hands.
+_STRICTER_VERSIONS_OF_HAND_RANKS = {
+    HandRank.ONE_PAIR: [
+      HandRank.TWO_PAIRS, HandRank.THREE_OF_A_KIND, HandRank.FULL_HOUSE, HandRank.FOUR_OF_A_KIND
+      ],
+    HandRank.TWO_PAIRS: [HandRank.FULL_HOUSE, HandRank.FOUR_OF_A_KIND],
+    HandRank.THREE_OF_A_KIND: [HandRank.FULL_HOUSE, HandRank.FOUR_OF_A_KIND],
+    HandRank.STRAIGHT: [HandRank.STRAIGHT_FLUSH, HandRank.ROYAL_FLUSH],
+    HandRank.FLUSH: [HandRank.STRAIGHT_FLUSH, HandRank.ROYAL_FLUSH],
+    HandRank.STRAIGHT_FLUSH: [HandRank.ROYAL_FLUSH],
+}
+
+def has_better_hand_than_hand_value(
+    hole_cards: HoleCards, board: Board, compare_to: HandValue) -> bool:
+  '''Takes a `HandValue` object and checks whether a better hand can be formed from the 7 cards
+  available to the player.
+
+  By using a `HandValue` object for the comparison instead of a set of cards makes it cheaper
+  to compare a hand against the hands of multiple opponents. This way we only need to evaluate
+  the hand's value once and can reuse the resulting `HandValue` for multiple comparisons.
+  '''
+  cards = hole_cards + board
+  checks_skipped = []
+  # It's better to start checking the weaker ranks first since lower hand ranks are more
+  # frequent thus it's more probable that we will find a better hand sooner this way.
+  for rank in sort_hand_ranks_in_incr_order():
+    if rank < compare_to.rank or rank in checks_skipped:
+      continue
+    check_for_rank = getattr(HandValueWhenRankIsFixedAs, rank.name)
+    hand_value = check_for_rank(cards)
+    if hand_value is None:
+      skip = _STRICTER_VERSIONS_OF_HAND_RANKS.get(rank, [])
+      checks_skipped.extend(skip)
+      continue
+    if _first_hand_value_is_stronger(hand_value, compare_to):
+      return True
+  return False
+
+
+def _first_hand_value_is_stronger(first: HandValue, second: HandValue) -> bool:
+  if first.rank == second.rank:
+    for (r1, r2) in zip(first.tie_breaker_card_ranks, second.tie_breaker_card_ranks):
+      if r1 == r2:
+        continue
+      return r1 > r2
+    # The HandValue is the same.
+    return False
+  else:
+    return first.rank > second.rank
---
+++ tests/test_compare_hands.py
@@ -0,0 +1,68 @@
+import unittest
+from itertools import permutations
+
+from texas_holdem.card import HoleCards, Board
+from texas_holdem.evaluate_hand import HandRank, HandValue, get_hand_value
+from texas_holdem.compare_hands import has_better_hand_than_hand_value
+from texas_holdem.shorthand_notations import *
+
+
+class TestHasBetterHandThenHandValue(unittest.TestCase):
+  'Tests for the function has_better_hand_than_hand_value.'
+  def test_works_when_comparing_different_ranks(self):
+    'Test cases where we compare a hand to a HandValue with different rank.'
+    # We want to check all combinations so for each hand rank we list 7 cards (the number of
+    # cards available for each player to form their hand) where the best hand is of the given
+    # rank. We compare all possible pairs among them.
+    cards_for_ranks = {
+        HandRank.HIGH_CARD: [H2, H3, S6, S7, C8, C10, DA],
+        HandRank.ONE_PAIR: [H2, H3, S6, S7, C7, C10, DA],
+        HandRank.TWO_PAIRS: [H2, H3, S6, C6, S7, C7, DA],
+        HandRank.THREE_OF_A_KIND: [H2, H3, S6, C6, D6, S7, DA],
+        HandRank.STRAIGHT: [H2, H3, S6, S7, C8, C9, D10],
+        HandRank.FLUSH: [H2, H3, S6, S7, S9, SJ, SA],
+        HandRank.FULL_HOUSE: [H2, H3, S6, C6, D6, S8, D8],
+        HandRank.FOUR_OF_A_KIND: [H2, H3, S6, C6, D6, H6, DA],
+        HandRank.STRAIGHT_FLUSH: [H2, H3, S6, S7, S8, S9, S10],
+        HandRank.ROYAL_FLUSH: [H2, H3, S10, SJ, SQ, SK, SA],
+    }
+    # calculate and store the `HandValue`s for the cards for each rank so we can reuse them
+    # during the comparisons.
+    hand_values = {}
+    for rank, cards in cards_for_ranks.items():
+      hole_cards = HoleCards(*cards[:2])
+      board = Board(*cards[2:])
+      hv = get_hand_value(hole_cards, board)
+      self.assertEqual( hv.rank, rank, msg=(
+            f'Cards {cards} are supposed to form a hand of rank {rank.name} but it is of ' +
+            f'{hv.rank.name}.')
+          )
+      hand_values[rank] = hv
+    for r1, r2 in permutations(cards_for_ranks.keys(), 2):
+      cards = cards_for_ranks[r1]
+      hole_cards = HoleCards(*cards[:2])
+      board = Board(*cards[2:])
+      compare_to = hand_values[r2]
+      is_the_hand_better = has_better_hand_than_hand_value(hole_cards, board, compare_to)
+      expected = r1 > r2
+      self.assertEqual(is_the_hand_better, expected, msg=(
+        f'Comparing whether the cards {cards} contain a better hand than {compare_to} should' +
+        f'return: {expected}.'))
+
+  def test_works_when_comparing_same_ranks(self):
+    board = [S5, C5, S7, D10, DA]
+    hole_cards = [CK, CA]  # Forms TWO_PAIRS with A and 5, with kicker K.
+
+    lower_hand_value = HandValue(
+        rank=HandRank.TWO_PAIRS,
+        tie_breaker_card_ranks=[Rank.ACE, Rank.FOUR, Rank.ACE])
+    equal_hand_value = HandValue(
+        rank=HandRank.TWO_PAIRS,
+        tie_breaker_card_ranks=[Rank.ACE, Rank.FIVE, Rank.KING])
+    stronger_hand_value = HandValue(
+        rank=HandRank.TWO_PAIRS,
+        tie_breaker_card_ranks=[Rank.ACE, Rank.SIX, Rank.TWO])
+
+    self.assertTrue(has_better_hand_than_hand_value(hole_cards, board, lower_hand_value))
+    self.assertFalse(has_better_hand_than_hand_value(hole_cards, board, equal_hand_value))
+    self.assertFalse(has_better_hand_than_hand_value(hole_cards, board, stronger_hand_value))
